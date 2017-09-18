0c32841241dd62cfde46760949f34ee248f39a04
Krystian Bacławski <cahirwpz@cs.uni.wroc.pl>
2017-09-18 16:18:55 +0200

    Add exception handlers.

---
+++ src/exc.c
@@ -0,0 +1,19 @@
+#define __interrupt(x) __attribute__ ((interrupt(x))) 
+
+__interrupt("UNDEF") void exc_undefined_instruction(void) {
+}
+
+__interrupt("SWI") void exc_supervisor_call(void) {
+}
+
+__interrupt("ABORT") void exc_prefetch_abort(void) {
+}
+
+__interrupt("ABORT") void exc_data_abort(void) {
+}
+
+__interrupt("IRQ") void exc_irq(void) {
+}
+
+__interrupt("FIQ") void exc_fast_irq(void) {
+}
