1d1ecae78b09c9538425cc2c9b9b0010da79754b
Krystian Bacławski <cahirwpz@cs.uni.wroc.pl>
2017-10-02 16:35:29 +0200

    Improve MMU data structures description.

--- include/armmmu.h
+++ include/armmmu.h
@@ -6,40 +6,42 @@
 /* ARMv6 page directory and page table entries */
 
 #define PDE_TYPE_FAULT 0
-#define PDE_TYPE_COARSE 1
+#define PDE_TYPE_TABLE 1
 #define PDE_TYPE_SECTION 2
 #define PDE_TYPE_RESERVED 3
 #define PDE_TYPE_MASK 3
 
 typedef struct {
-  union {
-    struct {
-      uint32_t type : 2;
-      uint32_t b : 1;
-      uint32_t c : 1;
-      uint32_t xn : 1;
-      uint32_t domain : 4;
-      uint32_t impl : 1;
-      uint32_t ap : 2;
-      uint32_t tex : 3;
-      uint32_t apx : 1;
-      uint32_t s : 1;
-      uint32_t ng : 1;
-      uint32_t zero : 1;
-      uint32_t ns : 1;
-      uint32_t base_address : 12;
-    } section;
-    struct {
-      uint32_t type : 2;
-      uint32_t pxn : 1;
-      uint32_t ns : 1;
-      uint32_t sbz : 1;
-      uint32_t domain : 4;
-      uint32_t impl : 1;
-      uint32_t base_address : 20;
-    } coarse;
-    uint32_t raw;
-  };
+  uint32_t type : 2;
+  uint32_t b : 1;
+  uint32_t c : 1;
+  uint32_t xn : 1;
+  uint32_t domain : 4;
+  uint32_t impl : 1;
+  uint32_t ap : 2;
+  uint32_t tex : 3;
+  uint32_t apx : 1;
+  uint32_t s : 1;
+  uint32_t ng : 1;
+  uint32_t zero : 1;
+  uint32_t ns : 1;
+  uint32_t base_address : 12;
+} pde_section_t;
+
+typedef struct {
+    uint32_t type : 2;
+    uint32_t pxn : 1;
+    uint32_t ns : 1;
+    uint32_t sbz : 1;
+    uint32_t domain : 4;
+    uint32_t impl : 1;
+    uint32_t base_address : 20;
+} pde_table_t;
+
+typedef union {
+  pde_table_t table;
+  pde_section_t section;
+  uint32_t raw;
 } pde_t;
 
 #define PTE_TYPE_FAULT 0
@@ -49,33 +51,35 @@ typedef struct {
 #define PTE_TYPE_MASK 3
 
 typedef struct {
-  union {
-    struct {
-      uint32_t type : 2;
-      uint32_t b : 1;
-      uint32_t c : 1;
-      uint32_t ap : 2;
-      uint32_t sbz : 3;
-      uint32_t apx : 1;
-      uint32_t s : 1;
-      uint32_t ng : 1;
-      uint32_t tex : 3;
-      uint32_t xn : 1;
-      uint32_t base_address : 16;
-    } large_page;
-    struct {
-      uint32_t type : 2;
-      uint32_t b : 1;
-      uint32_t c : 1;
-      uint32_t ap : 2;
-      uint32_t tex : 3;
-      uint32_t apx : 1;
-      uint32_t s : 1;
-      uint32_t ng : 1;
-      uint32_t base_address : 20;
-    } page;
-    uint32_t raw;
-  };
+  uint32_t type : 2;
+  uint32_t b : 1;
+  uint32_t c : 1;
+  uint32_t ap : 2;
+  uint32_t tex : 3;
+  uint32_t apx : 1;
+  uint32_t s : 1;
+  uint32_t ng : 1;
+  uint32_t base_address : 20;
+} pte_page_t;
+
+typedef struct {
+  uint32_t type : 2;
+  uint32_t b : 1;
+  uint32_t c : 1;
+  uint32_t ap : 2;
+  uint32_t sbz : 3;
+  uint32_t apx : 1;
+  uint32_t s : 1;
+  uint32_t ng : 1;
+  uint32_t tex : 3;
+  uint32_t xn : 1;
+  uint32_t base_address : 16;
+} pte_large_page_t;
+
+typedef union {
+  pte_page_t page;
+  pte_large_page_t large_page;
+  uint32_t raw;
 } pte_t;
 
 #endif // #ifndef ARMMMU_H
