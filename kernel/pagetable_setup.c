a24ebc988c92e490e2028358bbaef5be3b15b38c
Michal <michal.barnas83@gmail.com>
2019-08-25 20:32:05 +0200

    rpi set pagetable memory as non cachable

--- kernel/pagetable_setup.c
+++ kernel/pagetable_setup.c
@@ -3,6 +3,7 @@
 
 #define UPPERADDR 0xffffFFFF00000000
 #define PHYSADDR(x) ((x) - (UPPERADDR))
+#define PAGESIZE 4096
 extern uint64_t _level1_pagetable[];
 extern uint64_t _level2_pagetable[];
 extern uint64_t _level3_pagetable[];
@@ -27,10 +28,14 @@ page_table_fill_inner_nodes(void)
   entry_l2[0] = entry_l3_first2MB;
 }
 
-
 #define PTE_ATTR  L2_BLOCK | ATTR_SH(ATTR_SH_IS) | ATTR_NS | ATTR_AP(ATTR_AP_RW)
 #define ENTRY_2MB 0x00200000
 #define ENTRY_4KB 0x00001000
+
+extern int  _mail_buffer[];
+extern int  _bss_start[];
+extern int  _bss_end[];
+extern int  _brk_limit[];
 
 void __attribute__((section(".init")))
 page_table_fill_leaves(void)
@@ -39,27 +44,51 @@ page_table_fill_leaves(void)
   uint64_t *_level2_pagetable_phys = (void*)PHYSADDR((uint64_t)&_level2_pagetable);
   uint64_t *_level3_pagetable_phys = (void*)PHYSADDR((uint64_t)&_level3_pagetable);
 
-  // kernel page
-  entry = PTE_ATTR | ATTR_AF | ATTR_IDX(ATTR_NORMAL_MEM_NC) | L3_PAGE;
-  for(int i = 0; i < 512; entry += ENTRY_4KB){
+  // kernel page - first 2MB
+  int e = PHYSADDR((vaddr_t)&_bss_end) / PAGESIZE;
+  entry = PTE_ATTR | ATTR_AF | ATTR_IDX(ATTR_NORMAL_MEM_WB) | L3_PAGE;
+  for(int i = 0; i < e+46; entry += ENTRY_4KB){
     _level3_pagetable_phys[i++] = entry;
   }
- 
-  //first half (without first 2 MB) of first GB has write back cache 
-  entry = ENTRY_2MB | PTE_ATTR | ATTR_IDX(ATTR_NORMAL_MEM_WB);
-  for(int i = 1; i < 256; entry += ENTRY_2MB){
-    _level2_pagetable_phys[i++] = entry;
+
+  
+#define A PHYSADDR((vaddr_t)&_level1_pagetable) / PAGESIZE
+#define A2 PHYSADDR((vaddr_t)&_level2_pagetable) / PAGESIZE
+#define A3 PHYSADDR((vaddr_t)&_level3_pagetable) / PAGESIZE
+#define B PHYSADDR((vaddr_t)&_bss_end) / PAGESIZE
+#define C PHYSADDR((vaddr_t)&_mail_buffer) / PAGESIZE
+
+  entry = A*ENTRY_4KB | PTE_ATTR | ATTR_AF | L3_PAGE | ATTR_IDX(ATTR_NORMAL_MEM_NC);
+  _level3_pagetable_phys[A] = entry;
+  entry = A2*ENTRY_4KB | PTE_ATTR | ATTR_AF | L3_PAGE | ATTR_IDX(ATTR_NORMAL_MEM_NC);
+  _level3_pagetable_phys[A2] = entry;
+  entry = A3*ENTRY_4KB | PTE_ATTR | ATTR_AF | L3_PAGE | ATTR_IDX(ATTR_NORMAL_MEM_NC);
+  _level3_pagetable_phys[A3] = entry;
+
+  
+  entry = B*ENTRY_4KB | PTE_ATTR | ATTR_AF | L3_PAGE | ATTR_IDX(ATTR_NORMAL_MEM_NC);
+  _level3_pagetable_phys[B] = entry;
+
+  entry = C*ENTRY_4KB | PTE_ATTR | ATTR_AF | L3_PAGE | ATTR_IDX(ATTR_NORMAL_MEM_NC);
+  _level3_pagetable_phys[C] = entry;
+
+  
+  // page_table L2
+
+  /* begin (without first 2 MB) of first GB has no mapping  */
+  int z = 480;
+  for(int i = 1; i < z; entry += ENTRY_2MB){
+    _level2_pagetable_phys[i++] = 0x0;
   }
 
-  //second half of first GB is non cacheable 
-  entry = (256*ENTRY_2MB) | PTE_ATTR | ATTR_IDX(ATTR_NORMAL_MEM_NC);
-  for(int i = 256; i < 512; entry += ENTRY_2MB){
+  //end of first GB is non cacheable - last few needed MB
+  entry = (z*ENTRY_2MB) | PTE_ATTR | ATTR_IDX(ATTR_NORMAL_MEM_NC);
+  for(int i = z; i < 512; entry += ENTRY_2MB){
     _level2_pagetable_phys[i++] = entry;
   }
-
 
   //just one entry for arm mmio pheripherials
-  _level2_pagetable_phys = (void*)PHYSADDR((uint64_t)&_level2_pagetable[512]);
+  //_level2_pagetable_phys = (void*)PHYSADDR((uint64_t)&_level2_pagetable[512]);
   entry = (512*ENTRY_2MB) | ATTR_AF | PTE_ATTR | ATTR_IDX(ATTR_DEVICE_MEM);
-  _level2_pagetable_phys[0] = entry;
+  _level2_pagetable_phys[512] = entry;
 }
