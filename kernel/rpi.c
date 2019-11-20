3652d79dfe70ff2ed2e27bb285c3dc9c5d8ecfab
Paweł Jasiak <pawel@jasiak.xyz>
2019-11-20 15:56:32 +0100

    Move el3_only to C code.

--- kernel/rpi.c
+++ kernel/rpi.c
@@ -1,4 +1,8 @@
 #include "types.h"
+#include "aarch64/aarch64reg.h"
+#include "aarch64/pte.h"
+#include "dev/bcm2836reg.h"
+
 
 extern uint64_t _bss_start[];
 extern uint64_t _bss_end[];
@@ -40,4 +44,13 @@ void __attribute__((section(".init"))) setup_tmp_stack() {
                    "LDR X2, =_kernel\n"
                    "SUBS X1, X1, X2\n"
                    "MOV SP, X1\n");
+}
+
+void __attribute__((section(".init"))) el3_only() {
+  __asm__ volatile("MRS X0, SCR_EL3\n"
+                   "ORR X0, X0, #(1<<10)\n" // RW EL2 Execution state is AArch64.
+                   "ORR X0, X0, #(1<<0)\n" // NS EL1 is Non-secure world.
+                   "MSR SCR_EL3, X0\n" 
+                   "MOV X0, #0b01001\n" // DAIF=0000
+                   "MSR SPSR_EL3, X0\n"); // M[4:0]=01001 EL2h must match SCR_EL3.RW
 }
